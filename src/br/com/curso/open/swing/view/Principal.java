5ed4273723d51b45853a117d2d89b5816f93733d
Diego Trevisan <106852450+diegotrevisancc@users.noreply.github.com>
2024-04-18 14:03:06 -0300

    chore: added openswing jars and configurated it

--- src/br/com/curso/open/swing/view/Principal.java
+++ src/br/com/curso/open/swing/view/Principal.java
@@ -5,6 +5,18 @@
 package br.com.curso.open.swing.view;
 
 import br.com.curso.open.swing.util.Util;
+import br.com.curso.open.swing.util.Dominios;
+import java.awt.event.KeyEvent;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import org.openswing.swing.internationalization.java.XMLResourcesFactory;
+import org.openswing.swing.permissions.java.ButtonsAuthorizations;
+import org.openswing.swing.util.client.ClientSettings;
 
 /**
  *
@@ -142,6 +154,68 @@ public class Principal extends javax.swing.JFrame {
             }
         });
     }
+    
+        private void configurarOpenSwing() {
+
+        Hashtable domains = new Hashtable();
+        new Dominios(domains);
+
+        Hashtable xmlFiles = new Hashtable();
+        xmlFiles.put("PT_BR", "br/com/curso/open/swing/arquivos/Resources_pt_br.xml");
+        ClientSettings clientSettings = new ClientSettings(
+                new XMLResourcesFactory(xmlFiles, false),
+                domains,
+                new ButtonsAuthorizations()
+        );
+
+        try {
+            Properties props = new Properties();
+
+            props.put("logoString", "Curso");
+            props.setProperty("brightMode", "on");
+            props.setProperty("menuOpaque", "on");
+            props.setProperty("backgroundPattern", "off");
+            props.setProperty("drawSquareButtons", "on");
+            props.setProperty("backgroundColor", "235 247 254");
+
+            props.setProperty("windowDecoration", "false");
+
+            com.jtattoo.plaf.mcwin.McWinLookAndFeel.setTheme(props);
+            UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ClientSettings.AS_TAB = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
+        ClientSettings.LOOK_AND_FEEL_CLASS_NAME = "com.jtattoo.plaf.mcwin.McWinLookAndFeel";
+
+        ClientSettings.VIEW_BACKGROUND_SEL_COLOR = true;
+        ClientSettings.BUTTON_INSERT_IMAGE_NAME = "inserir.png";
+        ClientSettings.BUTTON_RELOAD_IMAGE_NAME = "reload.png";
+        ClientSettings.BUTTON_SAVE_IMAGE_NAME = "salvar.png";
+        ClientSettings.BUTTON_FILTER_IMAGE_NAME = "pesquisa.png";
+        ClientSettings.BUTTON_DELETE_IMAGE_NAME = "excluir.png";
+        ClientSettings.BUTTON_COPY_IMAGE_NAME = "copiar.png";
+        ClientSettings.BUTTON_EDIT_IMAGE_NAME = "editar.png";
+        ClientSettings.BUTTON_EXPORT_IMAGE_NAME = "exportar.png";
+        ClientSettings.BUTTON_IMPORT_IMAGE_NAME = "importar.png";
+
+        ClientSettings.VIEW_MANDATORY_SYMBOL = true;
+        ClientSettings.ALLOW_OR_OPERATOR = false;
+        ClientSettings.LIKE = "ilike";
+
+        ClientSettings.TREE_BACK = null;
+        ClientSettings.FILTER_PANEL_ON_GRID = false;
+        ClientSettings.SHOW_FILTER_SYMBOL = true;
+        ClientSettings.ASK_BEFORE_CLOSE = true;
+        ClientSettings.RELOAD_LAST_VO_ON_FORM = true;
+        ClientSettings.MAX_EXPORTABLE_ROWS = 2000000000;
+        ClientSettings.SHOW_EVENT_QUEUE_EXCEPTIONS = true;
+        ClientSettings.SHOW_FOCUS_BORDER_ON_FORM = false;
+
+        ClientSettings.getInstance().setLanguage("PT_BR");
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
